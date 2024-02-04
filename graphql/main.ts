3068225a37370a8a226f8889856886f7bc47fa44
d.lysakov <d.lysakov@upstorage.net>
2024-02-04 13:14:57 +0300

    moved to pages directory

--- graphql/main.ts
+++
@@ -1,9 +0,0 @@
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
-
-
-export const apolloClient = new ApolloClient({
-    uri: process.env.NEXT_PUBLIC_GRAPHQL_URL,
-    cache: new InMemoryCache(),
-  });
-
-
